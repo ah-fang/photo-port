a8ad68d73dc14fcf73566b54bac44ec1e1ba8296
Andrea Fang <arhughes43@gmail.com>
2022-07-26 12:03:26 -0700

    modal tests and package update gh

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Modal({ currentPhoto }) {
+function Modal({ onClose, currentPhoto }) {
     const {name, category, description, index} = currentPhoto; 
 
 
@@ -13,7 +13,7 @@ function Modal({ currentPhoto }) {
                     alt="current category" 
                 />
                 <p>{description}</p>
-                <button type="button">
+                <button onClick={onClose} type="button">
                     Close this modal
                 </button>
             </div>
--- src/components/PhotoList/__tests__/index.test.js
+++ src/components/PhotoList/__tests__/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import PhotoList from '../'
 
 afterEach(cleanup);
